b54d5a4c440adc75730d95d15c1722e56d2ceb89
axc450 <axc450@cs.bham.ac.uk>
2017-10-05 21:06:29 +0100

    CandyBot Base

---
+++ CandyBot.js
@@ -0,0 +1,222 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+var data = require("./data.json");
+const client = new Discord.Client();
+
+readyForPicking = []
+pickid = []
+const candyMsg = ":candy::candy::candy::candy::candy::candy::candy:\n**Random Candy Appeared!**\n_Type .pick to pick them up!_"
+
+///////////////////////  OPTIONS  /////////////////////
+
+const chance =		//CHANCE TO SPAWN
+const candyMin = 	//MINIMUM CANDY
+const candyMax =	//MAXIMUM CANDY
+const roleCost =	//COST OF ROLE
+const serverName =	//SERVER NAME
+const roleName =	//ROLE NAME
+const activeChannels = 	//ARRAY OF CHANNELS THE BOT IS ACTIVE IN
+
+///////////////////////////////////////////////////////
+
+client.login("MzYzNDM2MzUwMDA5MTE0NjQ0.DLXOrQ.JaiBzYwE5wXktqxR5N7miRJ41RU");
+
+client.on("ready", () => 
+{
+	console.log("Setting up " + serverName + " Bot...")
+
+	for(c in activeChannels)
+	{
+		readyForPicking.push([activeChannels[c], false])
+		pickid.push([activeChannels[c], 0])
+	}
+
+	console.log("Channel array populated: " + readyForPicking)
+	console.log("Channel array populated: " + pickid)
+	console.log(serverName + " Bot Started!");
+});
+
+client.on("message", (m) => 
+{
+	if(activeChannels.indexOf(m.channel.name) == -1){return;}
+	
+	if(m.author.bot)
+	{
+		if (m.content == candyMsg)
+		{
+			arraySet(pickid, m.channel.name, m.id);
+		}
+		return;
+	}
+
+	if(Math.random() < chance && !arrayFind(readyForPicking, m.channel.name) && !m.content.startsWith("."))
+	{
+		m.channel.send(candyMsg);
+		arraySet(readyForPicking, m.channel.name, true);
+	}
+	
+	if(!m.content.startsWith(".")){return;}
+
+	switch(m.content) 
+	{
+    		case ".pick": pick(m); break;
+		case ".candy": candy(m); break;
+		case ".help": help(m); break;
+		case ".buy": buy(m); break;
+		case ".lb": lb(m); break;
+	}
+});
+
+function pick(m)
+{
+	if(arrayFind(readyForPicking, m.channel.name))
+	{
+		m.channel.messages.find("id", arrayFind(pickid, m.channel.name)).delete();
+		amount = getRandomInt(candyMin, candyMax)
+		m.channel.send(m.author + " picked up __**" + amount + "**__ :candy: !");
+		updateData(m.author.id, amount)
+		arraySet(readyForPicking, m.channel.name, false)
+	}
+	
+	m.delete()
+}
+
+function candy(m)
+{
+	if(data.hasOwnProperty(m.author.id))
+	{
+		m.channel.send(m.author + " You have __**" + data[m.author.id] + "**__ candy!");	
+	}
+	else
+	{
+		m.channel.send(m.author + " You dont have any candy! :frowning: ")
+	}
+}
+
+function help(m)
+{
+	m.channel.send(":candy: **__Candy Bot Help!__** :candy:\n```.help	Shows this help menu\n.pick	Picks up candy\n.candy   Shows your current candy\n.lb      Shows the leaderboard\n.buy     Buys a " + serverName + " Halloween Role! (Cost " + roleCost + ")```_Made by Super for " + serverName + "_");
+}
+
+function buy(m)
+{
+	name = m.author.id
+	hasRole = m.member.roles.find("name", roleName)
+	role = m.guild.roles.find("name", roleName)
+	
+	if(hasRole)
+	{
+		m.channel.send(m.author + " You already have the role! :joy: ");
+		return;
+	}
+	if(!data.hasOwnProperty(name))
+	{
+		m.channel.send(m.author + " You dont have any candy! :frowning: ");
+		return;
+	}
+	if(data[name] < roleCost)
+	{
+		m.channel.send(m.author + " You dont have enough candy! :frowning: (Need **__" + roleCost + "__**, you have **__" + data[name] + "__**)")
+		return;
+	}
+	
+	data[name] = data[name]-roleCost
+	m.member.addRole(role, "Bot")
+	m.channel.send(m.author + " You have bought the Halloween 2017 Role! Keep trying to earn candy! :sparkles: ");
+	saveData();
+}
+
+function lb(m)
+{
+	array = sort()
+	lbStr = ":candy: **__Candy Bot Leaderboard!__** :candy:\n```"
+	for(i = 0; i<10; i++)
+	{
+		if(i>array.length-1){break;}
+		lbStr = lbStr + (i+1) + ".	" + format(array[i][0]) + "	" + idtoname(array[i][1]) + "\n"
+	}
+	lbStr = lbStr + "```"
+	m.channel.send(lbStr)
+}
+
+function updateData(name, amount)
+{
+	if(data.hasOwnProperty(name))
+	{
+		data[name] = data[name]+amount	
+	}
+	else
+	{
+		data[name] = amount
+	}
+	saveData()
+}
+
+function sort()
+{
+  	var sortedArray = [];
+
+  	for(var i in data)
+  	{
+		sortedArray.push([data[i], i]);
+	}
+
+	return sortedArray.sort((a,b) => b[0] - a[0]);
+}
+
+function format(n)
+{
+	l = n.toString();
+	if (l.length == 3)
+	{
+		return l+" "
+	}
+	if (l.length == 2)
+	{
+		return l+"  "
+	}
+	if (l.length == 1)
+	{
+		return l+"   "
+	}
+	return l;
+	
+}
+
+function idtoname(str)
+{
+	return client.users.find("id", str).username;
+}
+
+function saveData()
+{
+	fs.writeFile("data.json", JSON.stringify(data, null, 2), 'utf8', (error) => { if (error) {console.log("Something Went Wrong!")} })
+}
+
+function getRandomInt(min, max) 
+{
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function arraySet(a, k, v)
+{
+	for(i in a)
+	{
+		if (a[i][0] == k)
+		{
+			a[i][1] = v;
+			return;
+		}
+	}
+}
+
+function arrayFind(a, k)
+{
+	for(i in a)
+	{
+		if (a[i][0] == k)
+		{
+			return a[i][1];
+		}
+	}
+}
