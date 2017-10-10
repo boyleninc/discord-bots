957cf3b6bf213f70cab5a3c3d0405de4c74c8a01
axc450 <axc450@cs.bham.ac.uk>
2017-10-10 02:43:52 +0100

    CandyBot v1.1

--- CandyBot.js
+++ CandyBot.js
@@ -20,16 +20,18 @@
 // .buy     Attempts to buy the event role //
 /////////////////////////////////////////////
 
-///////////
-// Setup //
-///////////
+/////////////////////////
+// Setup - DO NOT EDIT //
+/////////////////////////
 
-const Discord           = require("discord.js");
-const fs                = require("fs");
-const client            = new Discord.Client();
-var data                = require("./data.json");
+const version			= 1.1
+const Discord           = require("discord.js")
+const fs                = require("fs")
+const client            = new Discord.Client()
+var data                = require("./data.json")
 var readyForPicking     = []
 var pickid              = []
+var channelsString		= ""
 
 //////////////////////////////
 // Bot Options - Edit These //
@@ -50,7 +52,7 @@ const candyMsg          = ":candy::candy::candy::candy::candy::candy::candy:\n**
 // Code - DO NOT EDIT //
 ////////////////////////
 
-client.login(discordID);	//Connects to discord via the token
+client.login(discordID)	//Connects to discord via the token
 
 client.on("ready", () =>	//When the bot connected
 {
@@ -60,51 +62,54 @@ client.on("ready", () =>	//When the bot connected
 	{
 		readyForPicking.push([activeChannels[c], false])
 		pickid.push([activeChannels[c], 0])
+		channelsString = channelsString + client.channels.find("name",activeChannels[c]) + " "
 	}
+
+	const superString = client.users.get("157591768177573888");	//Get Super tag
 
 	console.log("Channel array populated: " + readyForPicking)	//Log message
 	console.log("Channel array populated: " + pickid)			//Log message
-	console.log(serverName + " Bot Started!");					//Log message
-});
+	console.log(serverName + " Bot Started!")					//Log message
+})
 
 client.on("message", (m) =>		//When a message is sent
 {
-	if(activeChannels.indexOf(m.channel.name) == -1){return;}	//Exit if its an inactive channel
+	if(activeChannels.indexOf(m.channel.name) == -1){return}	//Exit if its an inactive channel
 	
 	if(m.author.bot)	//If its a bot message
 	{
 		if (m.content == candyMsg)	//If the bot dropped some candy
 		{
-			arraySet(pickid, m.channel.name, m.id);	//Store the message ID of the dropped candy message
+			arraySet(pickid, m.channel.name, m.id)	//Store the message ID of the dropped candy message
 		}
-		return;	//Exit
+		return	//Exit
 	}
 
 	if(Math.random() < chance && !arrayFind(readyForPicking, m.channel.name) && !m.content.startsWith("."))	//If the requirements to drop candy are met
 	{
-		m.channel.send(candyMsg);	//Drop candy
-		arraySet(readyForPicking, m.channel.name, true);	//Dont drop more until the candy has been picked
+		m.channel.send(candyMsg)	//Drop candy
+		arraySet(readyForPicking, m.channel.name, true)	//Dont drop more until the candy has been picked
 	}
 	
-	if(!m.content.startsWith(".")){return;}	//Exit if the message is not a CandyBot command
+	if(!m.content.startsWith(".")){return}	//Exit if the message is not a CandyBot command
 
 	switch(m.content) //Command List
 	{
-		case ".pick": pick(m); break;		//Pick
-		case ".candy": candy(m); break;		//Candy
-		case ".help": help(m); break;		//Help
-		case ".buy": buy(m); break;			//Buy
-		case ".lb": lb(m); break;			//Leaderboard
+		case ".pick": pick(m); break		//Pick
+		case ".candy": candy(m); break		//Candy
+		case ".help": help(m); break		//Help
+		case ".buy": buy(m); break			//Buy
+		case ".lb": lb(m); break			//Leaderboard
 	}
-});
+})
 
 function pick(m)	//Handles a .pick command
 {
 	if(arrayFind(readyForPicking, m.channel.name))	//If there has been candy dropped
 	{
-		m.channel.messages.find("id", arrayFind(pickid, m.channel.name)).delete();	//Delete the candy drop message
+		m.channel.messages.find("id", arrayFind(pickid, m.channel.name)).delete()	//Delete the candy drop message
 		amount = getRandomInt(candyMin, candyMax)									//Create a random amount of candy
-		m.channel.send(m.author + " picked up __**" + amount + "**__ :candy: !");	//Send the picked message
+		m.channel.send(m.author + " picked up __**" + amount + "**__ :candy: !")	//Send the picked message
 		updateData(m.author.id, amount)												//Update the users candy level
 		arraySet(readyForPicking, m.channel.name, false)							//Set the bot to start dropping candy
 	}
@@ -116,7 +121,7 @@ function candy(m)	//Handles a .candy command
 {
 	if(data.hasOwnProperty(m.author.id))	//If user has candy
 	{
-		m.channel.send(m.author + " You have __**" + data[m.author.id] + "**__ candy!");	//Display users candy
+		m.channel.send(m.author + " You have __**" + data[m.author.id] + "**__ candy!")	//Display users candy
 	}
 	else
 	{
@@ -126,7 +131,7 @@ function candy(m)	//Handles a .candy command
 
 function help(m)	//Handles a .help command
 {
-	m.channel.send(":candy: **__Candy Bot Help!__** :candy:\n```.help	Shows this help menu\n.pick	Picks up candy\n.candy   Shows your current candy\n.lb      Shows the leaderboard\n.buy     Buys a " + serverName + " Halloween Role! (Cost " + roleCost + ")```_Made by Super for " + serverName + "_");
+	m.channel.send(":candy: **__Candy Bot Help!__** :candy:\n```.help	Shows this help menu\n.pick	Picks up candy\n.candy   Shows your current candy\n.lb      Shows the leaderboard\n.buy     Buys a " + serverName + " Halloween Role! (Cost " + roleCost + ")```**CandyBot v" + version + "** active in: " + channelsString + "\n[_Made for " + serverName + "_] [_Created by " + superString + "_]")
 }
 
 function buy(m)	//Handles a .buy command
@@ -137,24 +142,24 @@ function buy(m)	//Handles a .buy command
 	
 	if(hasRole)	//If the user has the role
 	{
-		m.channel.send(m.author + " You already have the role! :joy: ");
-		return;
+		m.channel.send(m.author + " You already have the role! :joy: ")
+		return
 	}
 	if(!data.hasOwnProperty(name))	//If the user has candy
 	{
-		m.channel.send(m.author + " You dont have any candy! :frowning: ");
-		return;
+		m.channel.send(m.author + " You dont have any candy! :frowning: ")
+		return
 	}
 	if(data[name] < roleCost)	//If the user does not have enough candy
 	{
 		m.channel.send(m.author + " You dont have enough candy! :frowning: (Need **__" + roleCost + "__**, you have **__" + data[name] + "__**)")
-		return;
+		return
 	}
 	
 	data[name] = data[name]-roleCost	//Remove candy
 	m.member.addRole(role, "Bot")		//Give the user the role
-	m.channel.send(m.author + " You have bought the " + roleName + " Role! Keep trying to earn candy! :sparkles: ");
-	saveData();	//Save user candy data
+	m.channel.send(m.author + " You have bought the " + roleName + " Role! Keep trying to earn candy! :sparkles: ")
+	saveData()	//Save user candy data
 }
 
 function lb(m)	//Handles a .lb command
@@ -163,8 +168,15 @@ function lb(m)	//Handles a .lb command
 	lbStr = ":candy: **__Candy Bot Leaderboard!__** :candy:\n```"	//Starting leaderboard string
 	for(i = 0; i<10; i++)	//First 10 places
 	{
-		if(i>array.length-1){break;}	//Exit if less then 10 people exist in the JSON data
-		lbStr = lbStr + (i+1) + ".	" + format(array[i][0]) + "	" + idtoname(array[i][1]) + "\n"	//Form a leaderboard string
+		if(i>array.length-1){break}	//Exit if less then 10 people exist in the JSON data
+		if(i < 9)
+		{
+			lbStr = lbStr + (i+1) + ".    " + format(array[i][0]) + "	" + idtoname(array[i][1]) + "\n"	//Form Leaderboard String
+		}
+		else
+		{
+			lbStr = lbStr + (i+1) + ".   " + format(array[i][0]) + "	" + idtoname(array[i][1]) + "\n"	//Form Leaderboard String
+		}
 	}
 	lbStr = lbStr + "```"	//Add a suffix
 	m.channel.send(lbStr)	//Send the leaderboard string
@@ -185,38 +197,38 @@ function updateData(name, amount)	//Updates JSON data
 
 function sort()	//Sorts JSON data
 {
-  	var sortedArray = [];	//Create temp array
+  	var sortedArray = []	//Create temp array
 
   	for(var i in data)	//Search all JSON data
   	{
-		sortedArray.push([data[i], i]);	//Populate array
+		sortedArray.push([data[i], i])	//Populate array
 	}
 
-	return sortedArray.sort((a,b) => b[0] - a[0]);	//Sort array on sort function
+	return sortedArray.sort((a,b) => b[0] - a[0])	//Sort array on sort function
 }
 
 function format(n)	//Format string for data alignment
 {
-	l = n.toString();
+	l = n.toString()
 	if (l.length == 3)
 	{
-		return l+" "
+		return l + " "
 	}
 	if (l.length == 2)
 	{
-		return l+"  "
+		return l + "  "
 	}
 	if (l.length == 1)
 	{
-		return l+"   "
+		return l + "   "
 	}
 	
-	return l;
+	return l
 }
 
 function idtoname(str)	//Converts a user ID to a username
 {
-	return client.users.find("id", str).username;
+	return client.users.find("id", str).username
 }
 
 function saveData()	//Save JSON data
@@ -226,7 +238,7 @@ function saveData()	//Save JSON data
 
 function getRandomInt(min, max)	//Return random integer
 {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 function arraySet(a, k, v)	//Sets array dara
@@ -235,8 +247,8 @@ function arraySet(a, k, v)	//Sets array dara
 	{
 		if (a[i][0] == k)
 		{
-			a[i][1] = v;
-			return;
+			a[i][1] = v
+			return
 		}
 	}
 }
@@ -247,7 +259,7 @@ function arrayFind(a, k)	//Finds array data
 	{
 		if (a[i][0] == k)
 		{
-			return a[i][1];
+			return a[i][1]
 		}
 	}
 }
